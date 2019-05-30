86ffa21d4719ea70a1f7a2e1bf451d26eb8535cb
Cohen <dcoco91@gmail.com>
2019-05-30 16:20:02 -0400

    removed scoring from reset variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,24 +16,19 @@ var loss = {
         return this.score;
     }
 }
+var userGuesses = [];
 var isHidden = true;
 var attemptsLeft = 10;
 
 // This function resets the game, and creates a character for the user to try to guess
-function reset(attempts){
-    if (attempts < 1){
-        loss.add();
-    }
-    else{
-        wins.add();
-    }
-
+function reset(){
+  
     document.getElementById('u-guessed').innerText = "";
     document.getElementById('letter-guessed').innerText = "";
     var compKey = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var x = Math.floor((Math.random() * 26));
     var y = compKey[x];
-    // attemptsLeft = 10;
+    attemptsLeft = 10;   
     return y;
 
 }
@@ -46,10 +41,24 @@ function reset(attempts){
 
 //main
 
-compLetter = reset(attemptsLeft);
+
+//testing attempts left variables
+compLetter = reset();
 console.log(compLetter);
 
+
 document.onkeyup = function(event){
+
+    attemptsLeft--;
+    var userInput = event.key.toLowerCase();
+
+    if (userInput === compLetter){
+        document.getElementById('middleText').innerText = "You Win! But now I'm thinking of another letter...";
+        
+    }
+
+
+
 
 }
 
