c0092fe4d7313197e69ea26e535bdb8908744656
Cohen <dcoco91@gmail.com>
2019-06-02 12:53:30 -0400

    made some things bigger and more clear. Game now works from a logic perspective, new letter is picked after a win or loss and scores are updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,8 @@ let wins = {
         var el = document.getElementById('win');                                                            //grabs the win element
         el.innerText = this.score;                                                                          //changes win element text to score
         attemptsLeft = 10;                                                                                  //reset the attempts after a win
-        document.getElementById('guesses-left').innerText = attemptsLeft;                                   //updates the guesses left on screen
+        document.getElementById('guesses-left').innerText = attemptsLeft;    
+        compLetter = reset();                                                                               //updates the guesses left on screen
         return this.score;                                                                                  //returns the score
     }
 
@@ -24,6 +25,7 @@ let loss = {
         el.innerText = this.score;
         attemptsLeft = 10;
         document.getElementById('guesses-left').innerText = attemptsLeft; 
+        compLetter = reset();
         return this.score;
     }
 }
@@ -49,9 +51,8 @@ function clearLetters () {
 
 
 //main
-
-
 //testing attempts left variables
+
 compLetter = reset();
 console.log(compLetter);
 
@@ -59,13 +60,12 @@ console.log(compLetter);
 document.onkeyup = function(event){
 
     //creates a variable to grab userinput, and updates the section that says You picked
-
     let userInput = event.key.toLowerCase();
     document.getElementById('u-guessed').innerText = "You picked: "
     document.getElementById('middleText').innerText = "Guess what letter I'm thinking of...";   //ensures that the You win text changes back to guess what letter 
 
     //only run this code when there are attempts left. 
-    if (attemptsLeft > 0){
+    if (attemptsLeft > 1){
 
         attemptsLeft--;                                                                         //decrement attempts counter
         document.getElementById('guesses-left').innerText = attemptsLeft;                       //changes the text of attempts left
@@ -73,14 +73,11 @@ document.onkeyup = function(event){
         document.getElementById('letter-guessed').innerHTML = userGuesses;                      //puts the array on the screen
 
         if (userInput === compLetter){
-
             document.getElementById('middleText').innerText = "Alright, let's play again!";
             document.getElementById('u-guessed').innerText = "Press any key to continue";
             wins.add();
             clearLetters();
         }
-        
-
     }
     else {
         loss.add();
